9aa5dd05ceecc21645006b58c0c65c8abe5a0657
BlackSeamAndrey <107030387+BlackSeamAndrey@users.noreply.github.com>
2022-06-07 11:34:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,141 @@
+import pygame
+import time
+import random
+
+pygame.init()
+
+# RGB
+white = (200, 200, 200)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (230, 50, 80)
+green = (0, 200, 0)
+blue = (50, 153, 213)
+
+# All
+clock = pygame.time.Clock()
+
+snake_block = 20
+dis_width = 400
+dis_height = 400
+
+font_style = pygame.font.SysFont("bahnschrift", 20)
+score_font = pygame.font.SysFont("comicsansms", 30)
+
+dis = pygame.display.set_mode((dis_width, dis_height))
+pygame.display.set_caption('Игра змейка')
+
+
+def Your_score(score):
+    value = score_font.render("Счет: " + str(score), True, yellow)
+    dis.blit(value, [0, 0])
+
+
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, green, [x[0], x[1], snake_block, snake_block])
+
+
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [dis_width / 6, dis_height / 2.5])
+
+
+def gameLoop():
+    game_over = False
+    game_close = False
+
+    x1 = dis_width / 2
+    y1 = dis_height / 2
+
+    x1_change = 0
+    y1_change = 0
+
+    snake_speed = 13
+
+    snake_List = []
+    Length_of_snake = 1
+
+    foodx = round(random.randrange(0, dis_width - snake_block) / snake_block) * snake_block
+    foody = round(random.randrange(0, dis_height - snake_block) / snake_block) * snake_block
+
+    while not game_over:
+
+        while game_close == True:
+            dis.fill(white)
+            message("Нажми C для того, чтобы возобновить или Q для выхода", red)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    elif event.key == pygame.K_c:
+                        gameLoop()
+                    elif event.key == pygame.quit():
+                        game_over = True
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    y1_change = -snake_block
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN:
+                    y1_change = snake_block
+                    x1_change = 0
+
+        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+            game_close = True
+
+        # if x1 >= dis_width:
+        #     x1 = 0
+        # elif x1 < 0:
+        #     x1 = dis_width
+        # elif y1 >= dis_height:
+        #     y1 = 0
+        # elif y1 < 0:
+        #     y1 = dis_height
+
+        x1 += x1_change
+        y1 += y1_change
+        dis.fill(white)
+        pygame.draw.rect(dis, red, [foodx, foody, snake_block, snake_block])
+        snake_Head = []
+        snake_Head.append(x1)
+        snake_Head.append(y1)
+        snake_List.append(snake_Head)
+
+        if len(snake_List) > Length_of_snake:
+            del snake_List[0]
+
+        for x in snake_List[:-1]:
+            if x == snake_Head:
+                game_close = True
+
+        our_snake(snake_block, snake_List)
+        Your_score(Length_of_snake - 1)
+
+        pygame.display.update()
+
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, dis_width - snake_block) / 20) * 20
+            foody = round(random.randrange(0, dis_height - snake_block) / 20) * 20
+            Length_of_snake += 1
+            snake_speed -= 0.1
+
+        clock.tick(snake_speed)
+
+    pygame.quit()
+    quit()
+
+
+gameLoop()
